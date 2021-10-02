2bb18a30b822f4e8de0961c4cdc4b723309f445b
D3iven <59113234+D3iven@users.noreply.github.com>
2021-10-02 23:59:18 +0800

    Create LaggerRemoverTask.php

---
+++ src/Phqzing/LaggerRemover/LaggerRemoverTask.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Phqzing\LaggerRemover;
+
+use pocketmine\scheduler\Task;
+
+class LaggerRemoverTask extends Task {
+  
+  private $plugin;
+  
+  public function __construct(Loaader $plugin){
+    $this->plugin = $plugin;
+  }
+  
+  public function onRun(int $tick){
+    if($this->getConfig()->get("always-check") == false){
+      $this->plugin->getScheduler()->cancelTask($this->getTaskId());
+      return;
+    }
+    foreach($this->plugin->getServer()->getOnlinePlayers() as $players){
+      if(in_array($players->getName(), $this->plugin->getConfig()->get("whitelisted-players"))) return;
+      if($players->getPing() >= $this->plugin->getConfig()->get("max-ping")){
+        $message = $this->plugin->getConfig()->get("kick-message");
+        $message = str_replace("{ping}", $players->getPing(), $message);
+        $players->kick($message, false);
+      }
+    }
+  }
+}
